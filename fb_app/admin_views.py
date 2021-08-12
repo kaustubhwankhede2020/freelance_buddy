f6be808742fda49ee7573d198488fbb01aecf519
dwaipayangupta <dgupta200@gmail.com>
2021-08-12 14:44:25 +0400

    Database Migrated to Postgress

--- fb_app/admin_views.py
+++ fb_app/admin_views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.urls import reverse
+from fb_app import models
 
 # Create your views here.
 def render_dashboard(request):
-    return render(request, 'admin_templates/dashboard.html')
+    customers = models.Customer.objects.all()
+    context = {
+        "customers": customers,
+    }
+    return render(request, 'admin_templates/dashboard.html', context)
